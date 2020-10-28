2c9e8bf5d3f2edbc8d1ae435ee1e464d265c2926
MatthewVaccaro <matthew.tylor.vaccaro@gmail.com>
2020-10-28 18:12:48 -0500

    day one

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,12 +7,51 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0
+        self.running = False
+        self.sp = 7  # our stack pointer starts at the top of a 0-7 index
+        self.fl = 0b00000000  # all flags set to false on initialization
+        self.reg[self.sp] = 0xf4  # initialize stack pointer to RAM address f4
+
+    def call_stack(self, func):
+        branch_table = {
+            0b10000010: self.LDI,
+            0b01000111: self.PRN,
+            0b00000001: self.HLT,
+        }
+        if func in branch_table:
+            branch_table[func]()
+        else:
+            print('invalid function')
+            sys.exit(1)
+
+    def LDI(self):
+        reg_num = self.ram_read(self.pc+1)
+        value = self.ram_read(self.pc+2)
+        self.reg[reg_num] = value
+        self.pc += 3
+
+    def PRN(self):
+        reg_num = self.ram_read(self.pc+1)
+        print(self.reg[reg_num])
+        self.pc += 2
+
+    def HLT(self):
+        self.running = False
+        self.pc += 1
 
     def load(self):
         """Load a program into memory."""
-
+        file_path = sys.argv[1]
+        program = open(f"{file_path}", "r")
         address = 0
+        for line in program:
+            if line[0] == "0" or line[0] == "1":
+                command = line.split("#", 1)[0]
+                self.ram[address] = int(command, 2)
+                address += 1
 
         # For now, we've just hardcoded a program:
 
@@ -33,12 +72,19 @@ class CPU:
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
+        
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         #elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
+
+    def ram_read(self, MAR):
+        return self.ram[MAR]
+
+    def ram_write(self, MDR, MAR):
+        self.ram[MAR] = MDR
 
     def trace(self):
         """
@@ -60,6 +106,10 @@ class CPU:
 
         print()
 
+
     def run(self):
         """Run the CPU."""
-        pass
+        self.running = True
+        while self.running:
+            ir = self.ram[self.pc]
+            self.call_stack(ir)
