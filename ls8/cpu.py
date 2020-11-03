d450b439b1a5869145756bee9d70d361d30a863c
MatthewVaccaro <matthew.tylor.vaccaro@gmail.com>
2020-11-02 19:36:30 -0600

    done

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,11 +2,11 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
-        """Construct a new CPU."""
         self.ram = [0] * 256
         self.reg = [0] * 8
         self.pc = 0
@@ -19,7 +19,12 @@ class CPU:
         branch_table = {
             0b10000010: self.LDI,
             0b01000111: self.PRN,
+            0b10100010: self.MULT,
+            0b01000101: self.PUSH,
+            0b01000110: self.POP,
             0b00000001: self.HLT,
+            0b01010000: self.CALL,
+            0b00010001: self.RET
         }
         if func in branch_table:
             branch_table[func]()
@@ -42,7 +47,30 @@ class CPU:
         self.running = False
         self.pc += 1
 
-    def load(self):
+    def MULT(self):
+        self.alu('MULT', self.pc+1, self.pc+2)
+        self.pc += 3
+
+    def PUSH(self, value=None):
+        self.sp -= 1
+        if not value:
+            value = self.reg[self.ram_read(self.pc + 1)]
+        self.ram_write(value, self.reg[self.sp])
+        self.pc += 2
+
+    def POP(self):
+        value = self.ram[self.sp]
+        self.reg[self.ram[self.pc + 1]] = value
+        self.sp += 1
+        self.pc += 2
+
+    def ram_read(self, MAR):
+        return self.ram[MAR]
+
+    def ram_write(self, MDR, MAR):
+        self.ram[MAR] = MDR
+
+    def load(self, file_path):
         """Load a program into memory."""
         file_path = sys.argv[1]
         program = open(f"{file_path}", "r")
@@ -53,49 +81,30 @@ class CPU:
                 self.ram[address] = int(command, 2)
                 address += 1
 
-        # For now, we've just hardcoded a program:
+    def CALL(self):
+        self.reg[self.sp] -= 1
+        self.ram_write(self.reg[self.sp], self.pc + 1)
+        self.pc += 2
+        self.trace()
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
-
+    def RET(self):
+        self.pc = self.ram_read(self.reg[self.sp])
+        self.reg[self.sp] += 1
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
-        
-
-        if op == "ADD":
-            self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        if op == "MULT":
+            self.reg[self.ram[reg_a]] *= self.reg[self.ram[reg_b]]
         else:
             raise Exception("Unsupported ALU operation")
-
-    def ram_read(self, MAR):
-        return self.ram[MAR]
-
-    def ram_write(self, MDR, MAR):
-        self.ram[MAR] = MDR
 
     def trace(self):
         """
         Handy function to print out the CPU state. You might want to call this
         from run() if you need help debugging.
         """
-
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -105,7 +114,6 @@ class CPU:
             print(" %02X" % self.reg[i], end='')
 
         print()
-
 
     def run(self):
         """Run the CPU."""
